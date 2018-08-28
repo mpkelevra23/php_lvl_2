11c409e82425d1c5a45ccf2d41161ba37f6e7b08
Maksim Piatov <mpkelevra23@gmail.com>
2018-08-28 10:22:57 +0300

    lesson_2: homework

---
+++ app/web/lesson_2/controlType.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kelevra23
+ * Date: 8/27/18
+ * Time: 7:04 PM
+ */
+class MyClass
+{
+    public function names(array $names)
+        //Тип array
+    {
+        $res = "<ul>";
+        foreach ($names as $name) {
+            $res .= "<li>{$name}</li>";
+        }
+        return $res .= "</ul>";
+    }
+
+    public function otherClassTypeFunc(OtherClass $otherClass)
+    {//Тип OtherClass
+        return $otherClass->var1;
+    }
+}
+
+$obj = new MyClass();
+$names = [
+    'Иван Андреев',
+    'Олег Симонов',
+    'Андрей Ефремов',
+    'Алексей Самсонов'
+];
+
+echo $obj->names($names);
