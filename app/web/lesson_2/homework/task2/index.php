bc365c44f5eb1397ce5b74dfb35560a1920c2c2f
mpkelevra23 <mpkelevra23@gmail.com>
2019-10-16 16:56:59 +0300

    Добавил закрытые магические методы в трейт Singleton

--- app/web/lesson_2/homework/task2/index.php
+++ app/web/lesson_2/homework/task2/index.php
@@ -10,16 +10,28 @@ trait Singleton
 {
     private static $instance;
 
-    private function __construct()
-    {
-    }
-
     public static function getInstance()
     {
         if (empty(self::$instance)) {
             self::$instance = new self();
         }
         return self::$instance;
+    }
+
+    private function __construct()
+    {
+    }
+
+    private function __sleep()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    private function __clone()
+    {
     }
 }
 
