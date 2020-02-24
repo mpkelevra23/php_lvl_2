81636f3c37149363d820be91d9e8a04cfe4fecea
mpkelevra23 <mpkelevra23@gmail.com>
2020-02-24 17:31:39 +0300

    Урок 8 Примеры работы с SOAP с тестовым wsdl-документом

---
+++ app/web/lesson_8/practice/SOAP/index.php
@@ -0,0 +1,6 @@
+<?php
+// Создание SOAP-клиента по WSDL-документу
+$client = new SoapClient("http://users.bugred.ru/tasks/soap/WrapperSoapServer.php?wsdl");
+// Поcылка SOAP-запроса и получение результата
+$result = $client->doRegister("test23@test.test", "test23", "test23");
+var_dump($result);
